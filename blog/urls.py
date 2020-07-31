3ccc2cb0bd02d35af6c906de657165c62586814f
adam050986@yahoo.fr <W@c00l00>
2020-07-31 14:23:27 +0200

    Redefinition de la page "accueil" et boucle for en JS

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('accueil', views.home),
-    path('article/<int:id_article>', views.view_article),
+    path('articles/<int:id_articles>', views.view_articles),
     # path('articles/<str:tag>', views.list_articles_by_tag), 
     # path('articles/<int:year>/<int:month>', views.list_articles),  
     path('redirection', views.view_redirection),
--- blog/views.py
+++ blog/views.py
@@ -3,36 +3,46 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+one_year_in_seconds = 31536000
 
-a = chr(2017)
-b = chr(2018)
-c = chr(2019)
 
 def home(request):
     """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+
+    """boucle "for" en Javascript pour que le calcul se fasse coté client et non coté serveur"""
+
     return HttpResponse("""
-        <h1>En quelle année le support Django 1.8 s'est-t-il arrété ?</h1>
+        <meta charset="UTF-8">
+        <h1>Choisir un article entre 1 et 10</h1>
 
-        <a href="http://localhost:8000/blog/articles/""" + a + """">2017</a>
-        <a href="http://localhost:8000/blog/articles/2018">2018</a>
-        <a href="http://localhost:8000/blog/articles/2019">2019</a>
-    """)
+        <script>
+        for (var i = 0; i <= 11; i++) {
+            
+            document.write ('<a href="http://localhost:8000/blog/articles/' + i + '"> Article n°' + i + '</a><br>')
+        }
+        </script>
+    """
+    )
 
-def view_article(request, id_article):
-    """ 
+
+def view_articles(request, id_articles):
+    """
     Vue qui affiche un article selon son identifiant (ou ID, ici un numéro)
     Son ID est le second paramètre de la fonction (pour rappel, le premier
     paramètre est TOUJOURS la requête de l'utilisateur)
     """
-    return HttpResponse(
-        "Vous avez demandé l'article n° {0} !".format(id_article)    
-    )
+    
+    if id_articles == 0: # si id_article = 0, redirection
+        return redirect(view_redirection)
 
-def view_article(request, id_article):
-    if id_article > 100:
+    if id_articles < 10:  # si id_article inférieur a 10 : affichage
+        return HttpResponse(
+            "Vous avez demandé l'article n° {0} !".format(id_articles)    
+        )
+    else: # si id_article supérieur a 10 : erreur 404
         raise Http404
 
-    return redirect(view_redirection)
+    
 
 def view_redirection(request):
-    return HttpResponse("Vous avez été redirigé.")
+    return HttpResponse("L'article 0 n'existe pas !! Vous avez été redirigé.")
